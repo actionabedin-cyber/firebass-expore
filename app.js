23e7d1255f88ca3a446a25c2ee22e65012155a37
actionabedin-cyber <actionabedin@gmail.com>
2026-02-23 17:34:05 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAqtk2RZWI3IzTCqCGqGvSpF_IrTGH9JXA",
-  authDomain: "firebss-expore.firebaseapp.com",
+  authDomain: "firebass-expore.firebaseapp.com",
   projectId: "firebass-expore",
+  storageBucket: "firebass-expore.firebasestorage.app",
+  messagingSenderId: "928664882087",
   appId: "1:928664882087:web:1b090d9c58cff9043f0482"
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Sign Up
-window.signUp = function() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  createUserWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      document.getElementById("msg").innerText = "Signup Successful! এখন Sign In করুন";
-    })
-    .catch(err => {
-      document.getElementById("msg").innerText = err.message;
-    });
-}
-
-// Sign In
-window.signIn = function() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  signInWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      window.location.href = "home.html";
-    })
-    .catch(err => {
-      document.getElementById("msg").innerText = err.message;
-    });
-}
